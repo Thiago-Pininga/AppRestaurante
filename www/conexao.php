98f5e6c71cade533041ae2c8f3cd311860de6c34
thiago-pininga <piningadev@hotmail.com>
2025-05-25 13:27:00 -0300

    Atualiza configuração do banco de dados no docker-compose e ajusta conexão no PHP

--- www/conexao.php
+++ www/conexao.php
@@ -1,8 +1,8 @@
 <?php
 
-$hostname = 'localhost';
-$usuario = 'root';
-$senha = '';
+$hostname = 'db';
+$usuario = 'user';
+$senha = 'password';
 $database = 'restaurante';
 
 $mysqli = new mysqli($hostname, $usuario, $senha, $database);
@@ -11,4 +11,7 @@ $mysqli = new mysqli($hostname, $usuario, $senha, $database);
 if ($mysqli -> connect_errno) {
     echo 'Falha ao conectar: ('.$mysqli->connect_errno . ') ' . $mysqli -> connect_errno;
 }
+
+$mysqli->query("SET time_zone = '-03:00';");
+
 ?>
