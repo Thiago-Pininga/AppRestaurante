b7790f2cbaf17ca78b738191b0b41fac96c6c3a2
thiago-pininga <piningadev@hotmail.com>
2025-09-27 17:57:19 -0300

    Added logout botton on Header section.

--- www/header.php
+++ www/header.php
@@ -13,7 +13,8 @@ if (!isset($pagina)) {
                 <li><a href="index.php" class="<?= ($pagina == 'Início') ? 'active' : '' ?>">Início</a></li>
                 <li><a href="produtos.php" class="<?= ($pagina == 'Produtos') ? 'active' : '' ?>">Produtos</a></li>
                 <li><a href="adicionar.php" class="<?= ($pagina == 'Adicionar') ? 'active' : '' ?>">Adicionar Produto</a></li>
-                <li><a href="relatorio.php" class="<?= ($pagina == 'Relatório') ? 'active' : '' ?>">Relatório</a></li> <!-- Nova página Relatório -->
+                <li><a href="relatorio.php" class="<?= ($pagina == 'Relatório') ? 'active' : '' ?>">Relatório</a></li>
+                <li><a href="logout.php" style="background-color:rgba(189, 26, 26, 1)">Logout</a></li> <!-- Botão de logout -->
             </ul>
         </nav>
     </div>
